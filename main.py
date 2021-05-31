cd32602572235300bdd8c159e1785c250062e797
MylesTrump <>
2021-05-31 18:11:05 +0000

    fixed a typo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 def addressing(names, apt_num, str_num, str_name, city, province, post_code):
 
     # output
-    if "" in apt_num:
+    if "None" in apt_num:
         print("\n{0}".format(names))
         print("{0} {1}".format(str_num, str_name))
         print("{0} {1} {2}".format(city, province, post_code))
@@ -26,7 +26,7 @@ def main():
     # input
     names = input("Please input your full legal name: ")
     print("Please input your apartment number ", end="")
-    apt_num = input("(leave blank for none): ")
+    apt_num = input("(enter 'None' if none): ")
     str_num = input("Please input your street number: ")
     str_name = input("Please input your street name: ")
     city = input("Please input your city: ")
